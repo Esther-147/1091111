ccbc877f48ffeebfa7a64321b9af73a0f5552ffc
Esther-147 <71751338+Esther-147@users.noreply.github.com>
2020-11-11 16:08:20 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,9 @@
 basic.forever(function () {
     for (let X = 0; X <= 2; X++) {
         for (let Y = 0; Y <= 2; Y++) {
-            if (X == 2) {
-                led.plot(2, 2)
-            } else {
-                led.plot(1, 2)
-                led.plot(0, 2)
-                led.plot(3, 2)
-                led.plot(4, 2)
-            }
-            if (Y == 2) {
-                led.plot(2, 2)
-            } else {
-                led.plot(2, 1)
-                led.plot(2, 0)
-                led.plot(2, 3)
-                led.plot(2, 4)
+            if (2 - Y == 2 || 2 + (X - Y) == 2) {
+                led.plot(2 + Y, 2 - (X - Y))
+                led.plot(2 - Y, 2 + (X - Y))
             }
         }
         basic.pause(100)
